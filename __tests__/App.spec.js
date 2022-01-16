466581931d0fdfddd407cb00abf39a3ec39351ba
jackleett0505 <jackleett0505@outlook.com>
2022-01-16 09:45:09 +0100

    ci-cd-update

--- __tests__/App.spec.js
+++ __tests__/App.spec.js
@@ -3,22 +3,24 @@ expect.extend({ toMatchImageSnapshot });
 
 const puppeteer = require("puppeteer");
 
-it('renders correctly', async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-  // set the size of the viewport, so our screenshot will have the desired size
-  await page.setViewport({
-      width: 1024,
-      height: 768
+describe('screenshot test', () => {
+  it('renders correctly', async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+  
+    // set the size of the viewport, so our screenshot will have the desired size
+    await page.setViewport({
+        width: 1024,
+        height: 768
+    })
+  
+    await page.goto('http://localhost:8080/')
+  
+    const image = await page.screenshot({
+      path: "homepage.png",
+      fullPage: true
+    });
+  
+    expect(image).toMatchImageSnapshot();
   })
-
-  await page.goto('http://localhost:8080/')
-
-  const image = await page.screenshot({
-    path: "homepage.png",
-    fullPage: true
-  });
-
-  expect(image).toMatchImageSnapshot();
-});
+})
